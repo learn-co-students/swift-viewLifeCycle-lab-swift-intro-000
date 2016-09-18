4ef713af30e2a170a750ffb728ebdff37e1a5453
JimCampagno <jimcampagno@gmail.com>
2016-09-17 20:12:00 -0400

    Fix up possible error in Storyboard

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -2,25 +2,26 @@
 //  ViewController.swift
 //  FunWithViews
 //
-//  Created by James Campagno on 6/1/16.
+//  Created by Jim Campagno on 9/17/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
         
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        
     }
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
     }
-    
+
 }
