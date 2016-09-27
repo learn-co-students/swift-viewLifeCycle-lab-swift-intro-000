8f3e59fd323ceb2539cbdb7e715e7f583c8ac562
jajuki13 <khkim1776@gmail.com>
2016-09-26 22:18:32 -0400

    tinkered with the func for dieroll +1. because of index

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -15,13 +15,17 @@ class ViewController: UIViewController {
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        
-        
-    }
-    
-    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
-    func randomDiceRoll() -> Int {
-        return Int(arc4random_uniform(6) + 1)
+//        randomDiceRoll()
+        print(randomDiceRoll())
     }
 
+    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
+    func randomDiceRoll() -> Int {
+        var diceRoll = Int(arc4random_uniform(6) + 1)
+        if diceRoll >= 7 {
+            diceRoll -= 1
+        }
+        return diceRoll
+    }
+    
 }
