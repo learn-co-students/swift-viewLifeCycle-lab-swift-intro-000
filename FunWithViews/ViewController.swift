7e95112300434865b3f6558831f73b1c5d44da93
Robert Lowe <RobTheHuman@Roberts-MacBook-Pro.local>
2016-09-27 23:51:51 -0400

    Added all 13 UI buttons. Hid all labels and views

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var view5: UIView!
+    @IBOutlet weak var view6: UIView!
+    @IBOutlet weak var view7: UIView!
+    @IBOutlet weak var L1: UILabel!
+    @IBOutlet weak var L2: UILabel!
+    @IBOutlet weak var L3: UILabel!
+    @IBOutlet weak var L4: UILabel!
+    @IBOutlet weak var L5: UILabel!
+    @IBOutlet weak var L6: UILabel!
+    
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view1.isHidden = true
+        view2.isHidden = true
+        view3.isHidden = true
+        view4.isHidden = true
+        view5.isHidden = true
+        view6.isHidden = true
+        view7.isHidden = true
+        L1.isHidden = true
+        L2.isHidden = true
+        L3.isHidden = true
+        L4.isHidden = true
+        L5.isHidden = true
+        L6.isHidden = true
+        
     }
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
