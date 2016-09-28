854bf58e7f12341295e33b80f96464e1664d57c2
jajuki13 <khkim1776@gmail.com>
2016-09-27 20:51:22 -0400

    all views required have been layed out

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -15,17 +15,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-//        randomDiceRoll()
+        let valueRolled = randomDiceRoll()
         print(randomDiceRoll())
+        print(valueRolled)
     }
 
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
-        var diceRoll = Int(arc4random_uniform(6) + 1)
-        if diceRoll >= 7 {
-            diceRoll -= 1
-        }
-        return diceRoll
+        return Int(arc4random_uniform(6) + 1)
     }
     
 }
