06b8ec7db115db84c5de82bd531a42964f9f2828
Jacque DesMarais <jacquedes@gmail.com>
2016-12-18 12:58:54 -0600

    Completed update
    Final working solution

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -32,91 +32,129 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelFive: UILabel!
     @IBOutlet weak var labelSix: UILabel!
     
-    
     override func viewDidLoad() {
         
-            topLeft.isHidden = true
-            middleLeft.isHidden = true
-            bottomLeft.isHidden = true
-            middle.isHidden = true
-            topRight.isHidden = true
-            middleRight.isHidden = true
-            bottomRight.isHidden = true
+        topLeft.isHidden = true
+        middleLeft.isHidden = true
+        bottomLeft.isHidden = true
+        middle.isHidden = true
+        topRight.isHidden = true
+        middleRight.isHidden = true
+        bottomRight.isHidden = true
 
-            redSquare.isHidden = true
-            orangeSquare.isHidden = true
-            yellowSquare.isHidden = true
-            greenSquare.isHidden = true
-            blueSquare.isHidden = true
-            purpleSquare.isHidden = true
+        redSquare.isHidden = true
+        orangeSquare.isHidden = true
+        yellowSquare.isHidden = true
+        greenSquare.isHidden = true
+        blueSquare.isHidden = true
+        purpleSquare.isHidden = true
+        
+        labelOne.text = ""
+        labelTwo.text = ""
+        labelThree.text = ""
+        labelFour.text = ""
+        labelFive.text = ""
+        labelSix.text = ""
         
         super.viewDidLoad()
-    }
+        }
     
-    @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        let roll = randomDiceRoll()
-        updateLabelWithRoll(roll)
-        showDots(roll)
-    }
+    var rollCount = 0
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
     }
-   
-    func updateLabelWithRoll(_ roll: Int) {
-        let rolls = String(roll)
-        
-        if labelOne.text == "0" {
-            labelOne.text = rolls
-            labelOne.isHidden = false
-        } else if labelTwo.text == "0" {
-            labelTwo.text = rolls
-            labelTwo.isHidden = false
-        } else if labelThree.text == "0" {
-            labelThree.text = rolls
-            labelThree.isHidden = false
-        } else if labelFour.text == "0" {
-            labelFour.text = rolls
-            labelFour.isHidden = false
-        } else if labelFive.text == "0" {
-            labelFive.text = rolls
-            labelFive.isHidden = false
-        } else if labelSix.text == "0" {
-            labelSix.text = rolls
-            labelSix.isHidden = false
-        }
-    }
-
     
-    func showDots(_ roll: Int) {
-        if roll == 1 {
+    @IBAction func dieButtonTapped(_ sender: AnyObject) {
+        let diceRoll = randomDiceRoll()
+        print("The dice roll is", diceRoll)
+        rollCount = rollCount + 1
+        print("Your roll count is", rollCount)
+        
+        topLeft.isHidden = true
+        middleLeft.isHidden = true
+        bottomLeft.isHidden = true
+        middle.isHidden = true
+        topRight.isHidden = true
+        middleRight.isHidden = true
+        bottomRight.isHidden = true
+        
+        redSquare.isHidden = true
+        orangeSquare.isHidden = true
+        yellowSquare.isHidden = true
+        greenSquare.isHidden = true
+        blueSquare.isHidden = true
+        purpleSquare.isHidden = true
+    
+        switch diceRoll {
+        case 1:
+            middle.isHidden = false
+        case 2:
+            topLeft.isHidden = false
+            bottomRight.isHidden = false
+        case 3:
+            topLeft.isHidden = false
+            middle.isHidden = false
+            bottomRight.isHidden = false
+        case 4:
+            topLeft.isHidden = false
+            bottomLeft.isHidden = false
+            topRight.isHidden = false
+            middleRight.isHidden = false
+        case 5:
+            topLeft.isHidden = false
+            bottomLeft.isHidden = false
+            middle.isHidden = false
+            topRight.isHidden = false
+            bottomRight.isHidden = false
+        case 6:
             topLeft.isHidden = false
             middleLeft.isHidden = false
             bottomLeft.isHidden = false
             topRight.isHidden = false
             middleRight.isHidden = false
             bottomRight.isHidden = false
-        } else if roll == 2 {
-            middleLeft.isHidden = false
-            bottomLeft.isHidden = false
-            middle.isHidden = false
-            topRight.isHidden = false
-            middleRight.isHidden = false
-        } else if roll == 3 {
-            middleLeft.isHidden = false
-            bottomLeft.isHidden = false
-            topRight.isHidden = false
-            middleLeft.isHidden = false
-        } else if roll == 4 {
-            middleLeft.isHidden = false
-            middle.isHidden = false
-            middleRight.isHidden = false
-        } else if roll == 5 {
-            middleLeft.isHidden = false
-            middleRight.isHidden = false
-        } else if roll == 6 {
-            middle.isHidden = false
+        default:
+            print("You did it!")
+        }
+
+        switch rollCount {
+        case 1:
+            redSquare.isHidden = false
+            labelOne.text = String(diceRoll)
+        case 2:
+            redSquare.isHidden = false
+            orangeSquare.isHidden = false
+            labelTwo.text = String(diceRoll)
+        case 3:
+            redSquare.isHidden = false
+            orangeSquare.isHidden = false
+            yellowSquare.isHidden = false
+            labelThree.text = String(diceRoll)
+        case 4:
+            redSquare.isHidden = false
+            orangeSquare.isHidden = false
+            yellowSquare.isHidden = false
+            greenSquare.isHidden = false
+            labelFour.text = String(diceRoll)
+        case 5:
+            redSquare.isHidden = false
+            orangeSquare.isHidden = false
+            yellowSquare.isHidden = false
+            greenSquare.isHidden = false
+            blueSquare.isHidden = false
+            labelFive.text = String(diceRoll)
+        case 6:
+            redSquare.isHidden = false
+            orangeSquare.isHidden = false
+            yellowSquare.isHidden = false
+            greenSquare.isHidden = false
+            blueSquare.isHidden = false
+            purpleSquare.isHidden = false
+            labelSix.text = String(diceRoll)
+        default:
+            print("You go, Glen Coco!")
         }
     }
 }
