9675fe40fab6e83bc815751b16f280c85a5e4f4b
Terrence <Terrence@Terrences-iMac.local>
2017-01-19 18:51:08 -0500

    rollTheDice

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -10,37 +10,38 @@
  
  class ViewController: UIViewController {
     
-    @IBOutlet weak var G: UIView!
-    @IBOutlet weak var F: UIView!
-    @IBOutlet weak var E: UIView!
-    @IBOutlet weak var D: UIView!
-    @IBOutlet weak var C: UIView!
-    @IBOutlet weak var B: UIView!
-    @IBOutlet weak var A: UIView!
-    @IBOutlet weak var One: UILabel!
-    @IBOutlet weak var Two: UILabel!
-    @IBOutlet weak var Three: UILabel!
-    @IBOutlet weak var Four: UILabel!
-    @IBOutlet weak var Five: UILabel!
-    @IBOutlet weak var Six: UILabel!
+    @IBOutlet weak var upperLeft: UIView!
+    @IBOutlet weak var upperRight: UIView!
+    @IBOutlet weak var middleLeft: UIView!
+    @IBOutlet weak var middleSquare: UIView!
+    @IBOutlet weak var middleRight: UIView!
+    @IBOutlet weak var lowerLeft: UIView!
+    @IBOutlet weak var lowerRight: UIView!
+    @IBOutlet weak var darkBlueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var pinkLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var yellowLabel: UILabel!
+    @IBOutlet weak var lightBlueLabel: UILabel!
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         resetDiFace()
-        G.isHidden = true
-        F.isHidden = true
-        E.isHidden = true
-        D.isHidden = true
-        C.isHidden = true
-        B.isHidden = true
-        A.isHidden = true
-        One.isHidden = true
-        Two.isHidden = true
-        Three.isHidden = true
-        Four.isHidden = true
-        Five.isHidden = true
-        Six.isHidden = true
+        upperLeft.isHidden = true
+        upperRight.isHidden = true
+        middleLeft.isHidden = true
+        middleSquare.isHidden = true
+        middleRight.isHidden = true
+        lowerLeft.isHidden = true
+        lowerRight.isHidden = true
+        darkBlueLabel.isHidden = true
+        greenLabel.isHidden = true
+        pinkLabel.isHidden = true
+        redLabel.isHidden = true
+        yellowLabel.isHidden = true
+        lightBlueLabel.isHidden = true
     }
     
     
@@ -56,35 +57,35 @@
     }
     
     func resetDiFace(){
-        A.isHidden = true
-        B.isHidden = true
-        C.isHidden = true
-        D.isHidden = true
-        E.isHidden = true
-        F.isHidden = true
-        G.isHidden = true
+        upperLeft.isHidden = true
+        upperRight.isHidden = true
+        middleLeft.isHidden = true
+        middleSquare.isHidden = true
+        middleRight.isHidden = true
+        lowerLeft.isHidden = true
+        lowerRight.isHidden = true
     }
     
     func updateLabelWithRoll(roll: Int) {
         let diceScore = String(roll)
-        if One.isHidden {
-            One.text = diceScore
-            One.isHidden = false
-        } else if Two.isHidden {
-            Two.text = diceScore
-            Two.isHidden = false
-        } else if Three.isHidden {
-            Three.text = diceScore
-            Three.isHidden = false
-        } else if Four.isHidden {
-            Four.text = diceScore
-            Four.isHidden = false
-        } else if Five.isHidden {
-            Five.text = diceScore
-            Five.isHidden = false
-        } else if Six.isHidden {
-            Six.text = diceScore
-            Six.isHidden = false
+        if darkBlueLabel.isHidden {
+            darkBlueLabel.text = diceScore
+            darkBlueLabel.isHidden = false
+        } else if greenLabel.isHidden {
+            greenLabel.text = diceScore
+            greenLabel.isHidden = false
+        } else if pinkLabel.isHidden {
+            pinkLabel.text = diceScore
+            pinkLabel.isHidden = false
+        } else if redLabel.isHidden {
+            redLabel.text = diceScore
+            redLabel.isHidden = false
+        } else if yellowLabel.isHidden {
+            yellowLabel.text = diceScore
+            yellowLabel.isHidden = false
+        } else if lightBlueLabel.isHidden {
+            lightBlueLabel.text = diceScore
+            lightBlueLabel.isHidden = false
         }
     }
     
@@ -94,32 +95,32 @@
         
         switch true {
         case diValue == 1:
-            A.isHidden = false
+            middleSquare.isHidden = false
         case diValue == 2:
-            A.isHidden = false
-            B.isHidden = false
+            middleSquare.isHidden = false
+            upperLeft.isHidden = false
         case diValue == 3:
-            A.isHidden = false
-            B.isHidden = false
-            C.isHidden = false
+            upperLeft.isHidden = false
+            middleSquare.isHidden = false
+            lowerRight.isHidden = false
         case diValue == 4:
-            A.isHidden = false
-            B.isHidden = false
-            C.isHidden = false
-            D.isHidden = false
+            upperLeft.isHidden = false
+            upperRight.isHidden = false
+            lowerLeft.isHidden = false
+            lowerRight.isHidden = false
         case diValue == 5:
-            A.isHidden = false
-            B.isHidden = false
-            C.isHidden = false
-            D.isHidden = false
-            E.isHidden = false
+            upperLeft.isHidden = false
+            upperRight.isHidden = false
+            lowerLeft.isHidden = false
+            lowerRight.isHidden = false
+            middleSquare.isHidden = false
         case diValue == 6:
-            A.isHidden = false
-            B.isHidden = false
-            C.isHidden = false
-            D.isHidden = false
-            E.isHidden = false
-            G.isHidden = false
+            upperLeft.isHidden = false
+            upperRight.isHidden = false
+            lowerLeft.isHidden = false
+            lowerRight.isHidden = false
+            middleLeft.isHidden = false
+            middleRight.isHidden = false
         default:
             resetDiFace()
         }
