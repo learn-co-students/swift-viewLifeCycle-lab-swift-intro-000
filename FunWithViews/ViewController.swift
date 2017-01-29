d2b7615f88d950a13071fa65b5532448f61ce652
larenSakota <jedi.temple@yahoo.com>
2017-01-28 17:30:40 -0800

    restarted, and now working through different tests for each element

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -10,18 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result1: UILabel!
+    @IBOutlet weak var result2: UILabel!
+    @IBOutlet weak var result3: UILabel!
+    @IBOutlet weak var result4: UILabel!
+    @IBOutlet weak var result5: UILabel!
+    @IBOutlet weak var result6: UILabel!
+    
+    @IBOutlet weak var box1: UIView!
+    @IBOutlet weak var box2: UIView!
+    @IBOutlet weak var box3: UIView!
+    @IBOutlet weak var box4: UIView!
+    @IBOutlet weak var box5: UIView!
+    @IBOutlet weak var box6: UIView!
+    @IBOutlet weak var box7: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        
+        _ = randomDiceRoll()
+    
         
     }
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
+    }
+    
+    func updateResultWithRoll(roll: Int) {
+        let diceScore = String(roll)
+        
+        if result1.isHidden {
+            result1.text = diceScore
+            result1.isHidden = false
+        }
     }
 
 }
