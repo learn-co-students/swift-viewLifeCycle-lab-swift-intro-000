5bd73ecbe91a3ace5bdd73a3ebbdc78ed90fe17d
NateDukatz <natedukatz@gmail.com>
2017-04-09 22:27:08 -0600

    View Life Cycle Lab Complete

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -9,19 +9,177 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view1.isHidden = true
+        view2.isHidden = true
+        view3.isHidden = true
+        view4.isHidden = true
+        view5.isHidden = true
+        view6.isHidden = true
+        view7.isHidden = true
+        
+        label1.isHidden = true
+        label2.isHidden = true
+        label3.isHidden = true
+        label4.isHidden = true
+        label5.isHidden = true
+        label6.isHidden = true
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
         
+        let number = showView()
+        updateLabelWithRoll(roll: number)
+        
         
     }
+    
+    @IBOutlet weak var view1: UIView!
+    
+    @IBOutlet weak var view2: UIView!
+    
+    @IBOutlet weak var view3: UIView!
+    
+    @IBOutlet weak var view4: UIView!
+    
+    @IBOutlet weak var view5: UIView!
+    
+    @IBOutlet weak var view6: UIView!
+    
+    @IBOutlet weak var view7: UIView!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var label4: UILabel!
+    
+    @IBOutlet weak var label5: UILabel!
+    
+    @IBOutlet weak var label6: UILabel!
+    
+    
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
+    }
+    
+    func showView() -> Int{
+        let number = randomDiceRoll()
+        count += 1
+        
+        switch number{
+        case 1:
+            view1.isHidden = true
+            view2.isHidden = true
+            view3.isHidden = true
+            view4.isHidden = false
+            view5.isHidden = true
+            view6.isHidden = true
+            view7.isHidden = true
+            return number
+        case 2:
+            view1.isHidden = true
+            view2.isHidden = false
+            view3.isHidden = true
+            view4.isHidden = true
+            view5.isHidden = true
+            view6.isHidden = false
+            view7.isHidden = true
+            return number
+        case 3:
+            view1.isHidden = true
+            view2.isHidden = false
+            view3.isHidden = true
+            view4.isHidden = false
+            view5.isHidden = true
+            view6.isHidden = false
+            view7.isHidden = true
+            return number
+        case 4:
+            view1.isHidden = false
+            view2.isHidden = false
+            view3.isHidden = true
+            view4.isHidden = true
+            view5.isHidden = true
+            view6.isHidden = false
+            view7.isHidden = false
+            return number
+        case 5:
+            view1.isHidden = false
+            view2.isHidden = false
+            view3.isHidden = true
+            view4.isHidden = false
+            view5.isHidden = true
+            view6.isHidden = false
+            view7.isHidden = false
+            return number
+        case 6:
+            view1.isHidden = false
+            view2.isHidden = false
+            view3.isHidden = false
+            view4.isHidden = true
+            view5.isHidden = false
+            view6.isHidden = false
+            view7.isHidden = false
+            return number
+        default:
+            print("There has been a mistake")
+            return 0
+        }
+    }
+    
+    func updateLabelWithRoll(roll: Int) {
+        
+        let diceScore = String(roll)
+        
+        if label1.isHidden {
+            label1.text = diceScore
+            label1.isHidden = false
+        } else if label2.isHidden {
+            label2.text = diceScore
+            label2.isHidden = false
+        } else if label3.isHidden {
+            label3.text = diceScore
+            label3.isHidden = false
+        }
+        else if label4.isHidden {
+            label4.text = diceScore
+            label4.isHidden = false
+        }
+        else if label5.isHidden {
+            label5.text = diceScore
+            label5.isHidden = false
+        }
+        else if label6.isHidden {
+            label6.text = diceScore
+            label6.isHidden = false
+        }
+        else{
+            let firstColor = label1.backgroundColor
+            
+            
+            label1.text = label2.text
+            label1.backgroundColor = label2.backgroundColor
+            label2.text = label3.text
+            label2.backgroundColor = label3.backgroundColor
+            label3.text = label4.text
+            label3.backgroundColor = label4.backgroundColor
+            label4.text = label5.text
+            label4.backgroundColor = label5.backgroundColor
+            label5.text = label6.text
+            label5.backgroundColor = label6.backgroundColor
+            label6.text = diceScore
+            label6.backgroundColor = firstColor
+        }
     }
 
 }
