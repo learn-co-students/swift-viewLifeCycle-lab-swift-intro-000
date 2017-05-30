fa8eb06e800150f718c9d2185119237d7ad1ae98
Garett Arrowood <garettarrowood@gmail.com>
2017-05-30 17:02:32 -0400

    Create IBOutlets & hide on them viewDidLoad

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -12,13 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let allViews = [
+            dieTopLeft, dieMiddleLeft, dieBottomLeft,
+            dieMiddle,
+            dieTopRight, dieMiddleRight, dieBottomRight,
+            scoreBox1, scoreBox2, scoreBox3,
+            scoreBox4, scoreBox5, scoreBox6
+        ]
+        for view in allViews {
+            view?.isHidden = true
+        }
     }
-    
+
+    @IBOutlet weak var dieTopLeft: UIView!
+    @IBOutlet weak var dieMiddleLeft: UIView!
+    @IBOutlet weak var dieBottomLeft: UIView!
+    @IBOutlet weak var dieMiddle: UIView!
+    @IBOutlet weak var dieTopRight: UIView!
+    @IBOutlet weak var dieMiddleRight: UIView!
+    @IBOutlet weak var dieBottomRight: UIView!
+
+    @IBOutlet weak var scoreBox1: UIView!
+    @IBOutlet weak var scoreBox2: UIView!
+    @IBOutlet weak var scoreBox3: UIView!
+    @IBOutlet weak var scoreBox4: UIView!
+    @IBOutlet weak var scoreBox5: UIView!
+    @IBOutlet weak var scoreBox6: UIView!
+
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        
-        
     }
-    
+
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
