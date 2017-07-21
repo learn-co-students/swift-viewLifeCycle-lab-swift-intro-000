b07f8cbb21f0da04ae672c67b6399ece6dc0b6a3
JakeCormier <jake.r.cormier@gmail.com>
2017-07-21 16:06:15 -0600

    Need to add roll history and constraints

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -10,18 +10,89 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+
+    @IBOutlet weak var Die1: UIView!
+    @IBOutlet weak var Die2: UIView!
+    @IBOutlet weak var Die3: UIView!
+    @IBOutlet weak var Die4: UIView!
+    @IBOutlet weak var Die5: UIView!
+    @IBOutlet weak var Die6: UIView!
+    @IBOutlet weak var Die7: UIView!
+    
+    @IBOutlet weak var correctNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Die1.isHidden = true
+        Die2.isHidden = true
+        Die3.isHidden = true
+        Die4.isHidden = true
+        Die5.isHidden = true
+        Die6.isHidden = true
+        Die7.isHidden = true
+        correctNumber.isHidden = true
+
     }
-    
-    @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        
-        
-    }
-    
-    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
+       // print(Int(arc4random_uniform(6) + 1))
         return Int(arc4random_uniform(6) + 1)
     }
 
+    // Returns back a random Int (1, 2, 3, 4, 5, or 6)
+
+
+
+    func correctNumberDisplay() {
+        correctNumber.isHidden = false
+       return correctNumber.text = "\(randomDiceRoll())"
+    }
+    func whatDie(){
+        
+        if randomDiceRoll() == 1 {
+            Die4.isHidden = false
+            
+        } else if randomDiceRoll() == 2 {
+            Die3.isHidden = false
+            Die5.isHidden = false
+            
+        } else if randomDiceRoll() == 3 {
+            Die3.isHidden = false
+            Die4.isHidden = false
+            Die5.isHidden = false
+            
+        } else if randomDiceRoll() == 4 {
+            Die1.isHidden = false
+            Die3.isHidden = false
+            Die5.isHidden = false
+            Die7.isHidden = false
+            
+        }else if randomDiceRoll() == 5 {
+            Die1.isHidden = false
+            Die3.isHidden = false
+            Die4.isHidden = false
+            Die5.isHidden = false
+            Die7.isHidden = false
+            
+        }else if randomDiceRoll() == 6 {
+            Die1.isHidden = false
+            Die2.isHidden = false
+            Die3.isHidden = false
+            Die5.isHidden = false
+            Die6.isHidden = false
+            Die7.isHidden = false
+            
+        }
+    }
+    
+    @IBAction func buttonPress(_ sender: Any) {
+      //  randomDiceRoll()
+        whatDie()
+        //correctNumberDisplay()
+        //viewDidLoad()
+        
+    }
+    @IBAction func rollAgain(_ sender: Any) {
+        viewDidLoad()
+    }
 }
