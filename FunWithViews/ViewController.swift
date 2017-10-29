90ebfb3dac437bb4c96f006e04d1a739119301a9
shanetreynolds6 <shanetreynolds@icloud.com>
2017-10-28 22:48:18 -0400

    DiceGame

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -9,13 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Dots for dice roll
+    @IBOutlet weak var boxOne: UIView!
+    @IBOutlet weak var boxTwo: UIView!
+    @IBOutlet weak var boxThree: UIView!
+    @IBOutlet weak var boxFour: UIView!
+    @IBOutlet weak var boxFive: UIView!
+    @IBOutlet weak var boxSix: UIView!
+    @IBOutlet weak var boxSeven: UIView!
+    
+    //Roll result indicators
+    @IBOutlet weak var green: UILabel!
+    @IBOutlet weak var yellow: UILabel!
+    @IBOutlet weak var red: UILabel!
+    @IBOutlet weak var pink: UILabel!
+    @IBOutlet weak var purple: UILabel!
+    @IBOutlet weak var blue: UILabel!
 
+    @IBAction func rollDice(_ sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        boxOne.isHidden = true
+        boxTwo.isHidden = true
+        boxThree.isHidden = true
+        boxFour.isHidden = true
+        boxFive.isHidden = true
+        boxSix.isHidden = true
+        boxSeven.isHidden = true
+        
+        green.isHidden = true
+        blue.isHidden = true
+        red.isHidden = true
+        yellow.isHidden = true
+        pink.isHidden = true
+        purple.isHidden = true
+        
+        
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        
+        randomDiceRoll()
         
     }
     
