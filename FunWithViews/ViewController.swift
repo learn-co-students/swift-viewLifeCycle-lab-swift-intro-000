43169aaf00b3b4c9a50c0d90eefc7858f468ada0
shanetreynolds6 <shanetreynolds@icloud.com>
2017-10-29 18:44:37 -0400

    dice game complete

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -52,7 +52,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-        randomDiceRoll()
+        let result = randomDiceRoll()
+        dieFaceValue(result: result)
+        labelUpdate(result: result)
         
     }
     
@@ -60,5 +62,96 @@ class ViewController: UIViewController {
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
     }
+    
+    func dieFaceValue(result: Int) {
+        switch result {
+        
+        case 1:
+            boxOne.isHidden = false
+            boxTwo.isHidden = true
+            boxThree.isHidden = true
+            boxFour.isHidden = true
+            boxFive.isHidden = true
+            boxSix.isHidden = true
+            boxSeven.isHidden = true
+            
+        case 2:
+            boxOne.isHidden = true
+            boxTwo.isHidden = false
+            boxThree.isHidden = true
+            boxFour.isHidden = true
+            boxFive.isHidden = true
+            boxSix.isHidden = true
+            boxSeven.isHidden = false
+        
+        case 3:
+            boxOne.isHidden = false
+            boxTwo.isHidden = false
+            boxThree.isHidden = true
+            boxFour.isHidden = true
+            boxFive.isHidden = true
+            boxSix.isHidden = true
+            boxSeven.isHidden = false
+        
+        case 4:
+            boxOne.isHidden = true
+            boxTwo.isHidden = false
+            boxThree.isHidden = true
+            boxFour.isHidden = false
+            boxFive.isHidden = false
+            boxSix.isHidden = true
+            boxSeven.isHidden = false
+        
+        case 5:
+            boxOne.isHidden = false
+            boxTwo.isHidden = false
+            boxThree.isHidden = true
+            boxFour.isHidden = false
+            boxFive.isHidden = false
+            boxSix.isHidden = true
+            boxSeven.isHidden = false
+        
+        default:
+            boxOne.isHidden = true
+            boxTwo.isHidden = false
+            boxThree.isHidden = false
+            boxFour.isHidden = false
+            boxFive.isHidden = false
+            boxSix.isHidden = false
+            boxSeven.isHidden = false
+        
+        
+        }
+    }
 
+    func labelUpdate(result: Int) {
+            let score = String(result)
+    
+        if green.isHidden {
+            green.text = score
+            green.isHidden = false
+        
+        }else if yellow.isHidden {
+                yellow.text = score
+                yellow.isHidden = false
+            
+        }else if red.isHidden {
+                red.text = score
+                red.isHidden = false
+
+        }else if pink.isHidden {
+                pink.text = score
+                pink.isHidden = false
+        
+        }else if purple.isHidden {
+                purple.text = score
+                purple.isHidden = false
+        
+        }else if blue.isHidden {
+                blue.text = score
+                blue.isHidden = false
+            }
+        
+        
+}
 }
