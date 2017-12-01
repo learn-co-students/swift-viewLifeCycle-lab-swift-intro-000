214128d73b1e79446d3c7dff7cde93952952af2e
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2017-11-30 22:08:05 -0600

    finally works

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var view5: UIView!
     @IBOutlet weak var view6: UIView!
     @IBOutlet weak var view7: UIView!
+
     @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var labelTwo: UILabel!
     @IBOutlet weak var labelThree: UILabel!
@@ -24,36 +25,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelFive: UILabel!
     @IBOutlet weak var labelSix: UILabel!
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Views
-        view1.isHidden = true
-        view2.isHidden = true
-        view3.isHidden = true
-        view4.isHidden = true
-        view5.isHidden = true
-        view6.isHidden = true
-        view7.isHidden = true
 
         //labels
         labelOne.isHidden = true
         labelTwo.isHidden = true
         labelThree.isHidden = true
+        labelFour.isHidden = true
+        labelFive.isHidden = true
+        labelSix.isHidden = true
 
     }
-    
+
     @IBAction func dieButtonTapped(_ sender: AnyObject) {
-//        var  rollDice = randomDiceRoll()
+        //        let  rollDice:Int = randomDiceRoll()
+        //        updateLabelWithRoll(roll: rollDice)
         updateLabelWithRoll(roll: randomDiceRoll())
-
+        ////        updateDie()
+        //
     }
-    
+
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
     }
+    func viewsDisapear(){
+        //Views
+        view1.isHidden = true
+        view2.isHidden = false
+        view3.isHidden = true
+        view4.isHidden = false
+        view5.isHidden = true
+        view6.isHidden = false
+        view7.isHidden = true
+    }
+    func cleanLabels(){
 
+        labelTwo.text = " "
+        labelTwo.isHidden = true
+        labelThree.text = " "
+        labelThree.isHidden = true
+        labelFour.text = " "
+        labelFour.isHidden = true
+        labelFive.text = " "
+        labelFive.isHidden = true
+        labelSix.text = " "
+        labelSix.isHidden = true
+    }
     func updateLabelWithRoll(roll: Int) {
 
         let diceScore = String(roll)
@@ -67,7 +88,7 @@ class ViewController: UIViewController {
         } else if labelThree.isHidden {
             labelThree.text = diceScore
             labelThree.isHidden = false
-        } else if labelFour.isHidden {
+        }else if labelFour.isHidden {
             labelFour.text = diceScore
             labelFour.isHidden = false
         }else if labelFive.isHidden {
@@ -77,18 +98,12 @@ class ViewController: UIViewController {
             labelSix.text = diceScore
             labelSix.isHidden = false
         } else {
-            labelOne.text = diceScore
-            labelTwo.text = " "
-            labelTwo.isHidden = true
-            labelThree.text = " "
-            labelThree.isHidden = true
-            labelFour.text = " "
-            labelFour.isHidden = true
-            labelFive.text = " "
-            labelFive.isHidden = true
-            labelSix.text = " "
-            labelSix.isHidden = true
+            cleanLabels()
+            //                viewsDisapear()
+            //            labelOne.text = diceScore
         }
     }
 
 }
+
+
